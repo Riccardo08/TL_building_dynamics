7dea9c5df41df3dd5eda90d006b5949eaad43370
Riccardo08 <ric.messi17@gmail.com>
2021-12-18 19:32:13 +0100

    multistep mlp

--- mlp.py
+++ mlp.py
@@ -84,14 +84,14 @@ def define_period(df, time):
         l_val = int(l_train * 2)
         df_def = df[:int(l_train * 3)]
     if time == 'year':
-        l_train = int(0.6 * len(df))  # 31536 (per un anno)
-        l_val = int(l_train + 0.3 * len(df))  # da 31536 a 42048, cioè 10512 valori (per un anno)
+        l_train = int(0.8 * len(df))  # 31536 (per un anno)
+        l_val = int(l_train + 0.1 * len(df))  # da 31536 a 42048, cioè 10512 valori (per un anno)
         df_def = df
 
     return df_def, l_train, l_val
 
 # time = 'month'
-df, l_train, l_val = define_period(df, time='month')
+df, l_train, l_val = define_period(df, time='year')
 
 
 # ______________________________________Datasets_preprocessing__________________________________________________________
@@ -137,9 +137,9 @@ class MLP(nn.Module):
     # define model elements
     def __init__(self, n_features):
         super(MLP, self).__init__()
-        self.hidden1 = Linear(n_features, 240) # input to first hidden layer
+        self.hidden1 = Linear(n_features, 100) # input to first hidden layer
         self.act1 = ReLU()
-        self.hidden2 = Linear(240, 100)
+        self.hidden2 = Linear(100, 100)
         self.act2 = ReLU()
         self.hidden3 = Linear(100, 100)
         self.act3 = ReLU()
@@ -173,11 +173,11 @@ class MLP(nn.Module):
 
 
 # __________________________________________________TRAINING PHASE______________________________________________________
-train_batch_size = 240
+train_batch_size = 500
 train_data = TensorDataset(train_X, train_Y)
 train_dl = DataLoader(train_data, batch_size=train_batch_size, shuffle=False, drop_last=True)
 
-val_batch_size = 240
+val_batch_size = 500
 val_data = TensorDataset(val_X, val_Y)
 val_dl = DataLoader(val_data, batch_size=val_batch_size, shuffle=False, drop_last=True)
 
@@ -211,11 +211,11 @@ def train_model(model, epochs, train_dl, val_dl, optimizer, criterion, lr_schedu
         for x, label in train_dl:
             # h = model.init_hidden(train_batch_size) # since the batch is big enough, a stateless mode is used (also considering the possibility to shuffle the training examples, which increase the generalization ability of the network)
             # h = tuple([each.data for each in h])
-            x = x.reshape(-1, features)
+            x = x.reshape(-1, n_features)
             # x = x.view(x.size(0), -1)
             output = model(x.float())
             #label = label.unsqueeze(1) # utilizzo .unsqueeze per non avere problemi di dimensioni
-            loss_c = criterion(output.view(-1), label.float())
+            loss_c = criterion(output, label.float())
             optimizer.zero_grad()
             loss_c.backward()
             optimizer.step()
@@ -229,12 +229,12 @@ def train_model(model, epochs, train_dl, val_dl, optimizer, criterion, lr_schedu
         val_loss = []
         # h = model.init_hidden(val_batch_size)
         for inputs, labels in val_dl:
-            inputs = inputs.reshape(-1, features)
+            inputs = inputs.reshape(-1, n_features)
             # x = x.view(x.size(0), -1)
             # h = tuple([each.data for each in h])
             val_output = model(inputs.float())
             #val_labels = labels.unsqueeze(1) # CAPIRE SE METTERLO O NO
-            val_loss_c = criterion(val_output.view(-1), labels.float())
+            val_loss_c = criterion(val_output, labels.float())
             val_loss.append(val_loss_c.item())
         # VAL_LOSS.append(val_loss.item())
         VAL_LOSS.append(np.sum(val_loss)/val_batch_size)
@@ -278,7 +278,7 @@ def train_model(model, epochs, train_dl, val_dl, optimizer, criterion, lr_schedu
 #         print("epoch: ", epoch, "validation loss: ", valid_total_loss/val_batch_size)
 #     return train_loss, val_loss
 
-epochs = 80
+epochs = 450
 train_loss, val_loss = train_model(mlp, epochs=epochs, train_dl=train_dl, val_dl=val_dl, optimizer=optimizer, criterion=criterion, lr_scheduler='', mode='')
 
 # train_loss, val_loss = train_model(mlp, epochs=epochs, train_dl=train_dl, val_dl=val_dl, optimizer=optimizer, train_batch_size=train_batch_size, val_batch_size=val_batch_size, mode='')
@@ -291,13 +291,144 @@ plt.plot(val_loss,color='b', linewidth = 1, label = 'Validation Loss')
 plt.yscale('log')
 plt.ylabel('Loss (MSE)')
 plt.xlabel('Epoch')
-plt.xticks(np.arange(0, int(epochs), 10))
+plt.xticks(np.arange(0, int(epochs), 50))
 plt.grid(b=True, which='major', color='#666666', linestyle='-')
 plt.minorticks_on()
 plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
 plt.title("Multi-steps training VS Validation loss", size=15)
 plt.legend()
-# plt.savefig('immagini/2015/1_month/LSTM_Train_VS_Val_LOSS({}_epochs_lr_{}).png'.format(epochs, lr))
+# plt.savefig('immagini/2015/mlp/MLP_Train_VS_Val_LOSS({}_epochs_lr_{}).png'.format(epochs, lr))
+plt.show()
+
+
+# ____________________________________________________SAVE THE MODEL____________________________________________________
+period = 'year'
+year = '2015'
+# torch.save(mlp.state_dict(), 'MLP_train_on_' + period + '_' + str(year) + '_epochs_'+str(epochs)+'_lr_' + str(lr) + '.pth')
+# torch.save(lstm.state_dict(), 'train_on_year_2015_epochs_150_lr_0.008.pth')
+
+# Load a model
+# model = MLP(n_features)
+# period = 'week'
+# year = '2015'
+# model_epochs = 190
+# model_lr = 0.009
+# model.load_state_dict(torch.load('train_on_'+period+'_'+year+'_epochs_'+str(model_epochs)+'_lr_'+str(model_lr)+'.pth'))
+# model.load_state_dict(torch.load('train_on_10_years_2015_epochs_25_lr_0.008_batch_2000.pth'))
+
+# __________________________________________________6h PREDICTION TESTING_______________________________________________
+
+test_batch_size = 400 # devo mettere il batch ad 1 perchè così ad ogni batch mi appendo il primo dei 6 valori predetti
+test_data = TensorDataset(test_X, test_Y)
+test_dl = DataLoader(test_data, shuffle=False, batch_size=test_batch_size, drop_last=True)
+
+
+def test_model(model, test_dl, maxT, minT, batch_size):
+    model.eval()
+    # h = model.init_hidden(batch_size)
+    y_pred = []
+    y_lab = []
+    y_lab6 = []
+    y_pred6 = []
+
+    for inputs, labels in test_dl:
+        # h = tuple([each.data for each in h])
+        # test_output, h = model(inputs.float(), h)
+        # labels = labels.unsqueeze(1)
+
+        inputs = inputs.reshape(-1, n_features)
+        output = model(inputs.float())
+        # loss = F.mse_loss(output.view(-1), labels.float())
+
+        # RESCALE OUTPUTS
+        output = output.detach().numpy()
+        # # test_output = np.reshape(test_output, (-1, 1))
+        test_output = minT + output*(maxT-minT)
+
+        # RESCALE LABELS
+        labels = labels.detach().numpy()
+        # # labels = np.reshape(labels, (-1, 1))
+        labels = minT + labels*(maxT-minT)
+
+        y_pred.append(test_output[:, 0]) # test_output[0] per appendere solo il primo dei valori predetti ad ogni step
+        y_lab.append(labels[:, 0]) # labels[0] per appendere solo il primo dei valori predetti ad ogni step
+        y_pred6.append(test_output[:, 5])
+        y_lab6.append(labels[:, 5])
+    return y_pred, y_lab, y_pred6, y_lab6
+
+
+y_pred, y_lab, y_pred6, y_lab6 = test_model(model=mlp, test_dl=test_dl, maxT=max_T, minT=min_T, batch_size=test_batch_size)
+
+
+flatten = lambda l: [item for sublist in l for item in sublist]
+y_pred = flatten(y_pred)
+y_lab = flatten(y_lab)
+y_pred = np.array(y_pred, dtype=float)
+y_lab = np.array(y_lab, dtype=float)
+#
+# y_pred6 = flatten(y_pred6)
+# y_lab6 = flatten(y_lab6)
+# y_pred6 = np.array(y_pred6, dtype=float)
+# y_lab6 = np.array(y_lab6, dtype=float)
+#
+# # Shift values of 6 positions because it's the sixth hour
+# y_pred6 = pd.DataFrame(y_pred6)
+# y_pred6 = y_pred6.shift(6, axis=0)
+# y_lab6 = pd.DataFrame(y_lab6)
+# y_lab6 = y_pred6.shift(6, axis=0)
+
+
+
+error = []
+error = y_pred - y_lab
+
+plt.hist(error, 100, linewidth=1.5, edgecolor='black', color='orange')
+plt.xticks(np.arange(-0.6, 0.6, 0.1))
+plt.xlim(-0.6, 0.6)
+plt.title('LSTM model 6h prediction error')
+# plt.xlabel('Error')
+plt.grid(True)
+# plt.savefig('immagini/2015/mlp/MLP_model_error({}_epochs_lr_{}_with_sixth_hour).png'.format(epochs, lr))
+plt.show()
+
+
+plt.plot(y_pred, color='orange', label="Predicted")
+plt.plot(y_lab, color="b", linestyle="dashed", linewidth=1, label="Real")
+# plt.plot(y_pred6, color='green', label="Predicted6")
+# plt.plot(y_lab6, color="b", linewidth=1, label="Real6")# , linestyle="purple"
+plt.grid(b=True, which='major', color='#666666', linestyle='-')
+plt.minorticks_on()
+plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+plt.xlim(0, 600)
+plt.ylabel('Mean Air Temperature [°C]')
+plt.xlabel('Time [h]')
+plt.title("6h prediction: Real VS predicted temperature", size=15)
+plt.legend()
+# plt.savefig('immagini/2015/mlp/MLP_real_VS_predicted_temperature({}_epochs_lr_{}_with_sixth_hour).png'.format(epochs, lr))
+plt.show()
+
+
+# METRICS
+MAPE = mean_absolute_percentage_error(y_lab, y_pred)
+MSE = mean_squared_error(y_lab, y_pred)
+R2 = r2_score(y_lab, y_pred)
+
+print('MAPE:%0.5f%%'%MAPE)
+print('MSE:', MSE.item())
+print('R2:', R2.item())
+
+
+plt.scatter(y_lab, y_pred,  color='k', edgecolor= 'white', linewidth=1, alpha=0.5)
+plt.text(18, 24.2, 'MAPE: {:.3f}'.format(MAPE), fontsize=15, bbox=dict(facecolor='red', alpha=0.5))
+plt.text(18, 26.2, 'MSE: {:.3f}'.format(MSE), fontsize=15, bbox=dict(facecolor='green', alpha=0.5))
+plt.plot([18, 28], [18, 28], color='red')
+plt.grid(b=True, which='major', color='#666666', linestyle='-')
+plt.minorticks_on()
+plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+plt.xlabel('Real Temperature [°C]')
+plt.ylabel('Predicted Temperature [°C]')
+plt.title("6h prediction: Prediction distribution", size=15)
+# plt.savefig('immagini/2015/mlp/MLP_prediction_distribution({}_epochs_lr_{}_with_sixth_hour).png'.format(epochs, lr))
 plt.show()
 
 
